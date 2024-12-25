59bff60268a4dc3beb99fa8534d4c82500dd94d7
MyOptimalNext <loukmanehadjsaid56@gmail.com>
2024-12-25 22:44:00 +0100

    Create CCPhaseGate.js

---
+++ gates/controlled_gates/CCPhaseGate.js
@@ -0,0 +1,31 @@
+class CCPhaseGate {
+  constructor(phi) {
+    this.matrix = [
+      [1, 0, 0, 0, 0, 0, 0, 0],
+      [0, 1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, Math.exp(Math.i * phi), 0, 0, 0],
+      [0, 0, 0, 0, 0, Math.exp(Math.i * phi), 0, 0],
+      [0, 0, 0, 0, 0, 0, Math.exp(Math.i * phi), 0],
+      [0, 0, 0, 0, 0, 0, 0, Math.exp(Math.i * phi)]
+    ];
+  }
+
+  apply(controlQubit1, controlQubit2, targetQubit) {
+    const qubits = [controlQubit1, controlQubit2, targetQubit];
+    return this.multiplyMatrix(qubits);
+  }
+
+  multiplyMatrix(qubits) {
+    let result = Array(8).fill(0);
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        result[i] += this.matrix[i][j] * qubits[j];
+      }
+    }
+    return result;
+  }
+}
+
+module.exports = CCPhaseGate;
