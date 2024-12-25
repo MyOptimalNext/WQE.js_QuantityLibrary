73f22dab3d759b99281223d58acffa722fc1104d
MyOptimalNext <loukmanehadjsaid56@gmail.com>
2024-12-25 22:38:52 +0100

    Create CZGate.js

---
+++ gates/controlled_gates/CZGate.js
@@ -0,0 +1,27 @@
+class CZGate {
+  constructor() {
+    this.matrix = [
+      [1, 0, 0, 0],
+      [0, 1, 0, 0],
+      [0, 0, -1, 0],
+      [0, 0, 0, -1]
+    ];
+  }
+
+  apply(controlQubit, targetQubit) {
+    const qubits = [controlQubit, targetQubit];
+    return this.multiplyMatrix(qubits);
+  }
+
+  multiplyMatrix(qubits) {
+    let result = [0, 0, 0, 0];
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        result[i] += this.matrix[i][j] * qubits[j];
+      }
+    }
+    return result;
+  }
+}
+
+module.exports = CZGate;
