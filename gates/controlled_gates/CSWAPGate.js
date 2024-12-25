65982978b5b7a2304f6171155d3a49cc9a3b9706
MyOptimalNext <loukmanehadjsaid56@gmail.com>
2024-12-25 22:40:50 +0100

    Create CSWAPGate.js

---
+++ gates/controlled_gates/CSWAPGate.js
@@ -0,0 +1,31 @@
+class CSWAPGate {
+  constructor() {
+    this.matrix = [
+      [1, 0, 0, 0, 0, 0, 0, 0],
+      [0, 1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 1]
+    ];
+  }
+
+  apply(controlQubit, targetQubit1, targetQubit2) {
+    const qubits = [controlQubit, targetQubit1, targetQubit2];
+    return this.multiplyMatrix(qubits);
+  }
+
+  multiplyMatrix(qubits) {
+    let result = Array(8).fill(0);
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        result[i] += this.matrix[i][j] * qubits[j];
+      }
+    }
+    return result;
+  }
+}
+
+module.exports = CSWAPGate;
