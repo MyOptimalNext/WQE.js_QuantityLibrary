766a24c698888a243e26544c363ec1fa7cddec07
MyOptimalNext <loukmanehadjsaid56@gmail.com>
2024-12-26 19:13:53 +0100

    Update XGate.js

--- gates/single_qubit/XGate.js
+++ gates/single_qubit/XGate.js
@@ -1,21 +1,9 @@
-class XGate {
-  constructor() {
-    this.matrix = [
-      [0, 1],
-      [1, 0]
-    ];
-  }
-
-  apply(qubit) {
-    return this.multiplyMatrix(qubit);
-  }
-
-  multiplyMatrix(qubit) {
-    let result = [0, 0];
-    result[0] = this.matrix[0][0] * qubit[0] + this.matrix[0][1] * qubit[1];
-    result[1] = this.matrix[1][0] * qubit[0] + this.matrix[1][1] * qubit[1];
-    return result;
-  }
+export default function XGate(qubit) {
+    if (qubit === 0) {
+        return 1;  // إذا كانت الحالة 0، تصبح 1
+    } else if (qubit === 1) {
+        return 0;  // إذا كانت الحالة 1، تصبح 0
+    } else {
+        throw new Error("حالة qubit غير صالحة. يجب أن تكون 0 أو 1");
+    }
 }
-
-export default XGate;
