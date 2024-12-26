1e568deb5d048df8ed703c0ebdf90bb68f00812a
MyOptimalNext <loukmanehadjsaid56@gmail.com>
2024-12-26 07:50:12 +0100

    Create YYGate.js

---
+++ gates/composite_gates/YYGate.js
@@ -0,0 +1,15 @@
+class YYGate {
+  constructor(angle) {
+    this.angle = angle;
+  }
+
+  apply(qubits) {
+    const cos = Math.cos(this.angle / 2);
+    const sin = Math.sin(this.angle / 2);
+
+    qubits[0] = cos * qubits[0] - sin * qubits[1];
+    qubits[1] = cos * qubits[1] - sin * qubits[0];
+  }
+}
+
+export default YYGate;
